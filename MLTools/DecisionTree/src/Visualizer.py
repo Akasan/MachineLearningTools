6c76ecabe8cee55b62708974397aef6a456cbed8
赤川大空 <daisuke1024akagawa@gmail.com>
2020-05-20 22:19:59 +0900

    add visualize options

--- MLTools/DecisionTree/src/Visualizer.py
+++ MLTools/DecisionTree/src/Visualizer.py
@@ -2,19 +2,54 @@ from graphviz import Digraph
 import pydotplus
 from sklearn.externals.six import StringIO
 from sklearn import tree
+from ._SaveGraph import write_file
 
 
 # sklearn標準の可視化手法を使う
 
 def _save_png(graph, filename):
+    """ save tree as png file
+
+    Arguments:
+    ----------
+        graph {pydotplus.graphviz.Dot} -- graph data
+        filename {str} -- file name
+
+    Examples:
+    ---------
+        >>> _save_png(graph, "hoge.png")        # the graph will be saves as hoge.png
+    """
     graph.write_png(filename)
 
 
 def _save_pdf(graph, filename):
+    """ save tree as pdf file
+
+    Arguments:
+    ----------
+        graph {pydotplus.graphviz.Dot} -- graph data
+        filename {str} -- file name
+
+    Examples:
+    ---------
+        >>> _save_png(graph, "hoge.pdf")        # the graph will be saves as hoge.pdf
+    """
     graph.write_pdf(filename)
 
 
 def save_file(graph, filename):
+    """ save graph to file. File type will be decided according to extension.
+
+    Arguments:
+    ----------
+        graph {pydotplus.graphviz.Dot} -- graph data
+        filename {str} -- file name
+
+    Examples:
+    ---------
+        >>> save_file(graph, "hoge.png")        # the graph will be saved as hoge.png
+        >>> save_file(graph, "hoge.pdf")        # the graph will be saved as hoge.pdf
+    """
     if filename.split(".")[-1] in ("PNG", "png"):
         _save_png(graph, filename)
 
@@ -32,10 +67,11 @@ def visualize_tree(clf, feature_name, method, depth=-1, format="png"):
     if not method == "Random Forest":
         dot_data = tree.export_graphviz(clf, out_file=None, feature_names=feature_name)
         graph = pydotplus.graph_from_dot_data(dot_data)
-        save_file(graph, "graph.pdf")
+        write_file(graph, "graph.gif", "gif")
 
     else:
         for i, _clf in enumerate(clf.estimators_):
             dot_data = tree.export_graphviz(_clf, out_file=None, feature_names=feature_name)
             graph = pydotplus.graph_from_dot_data(dot_data)
+            print(dir(graph))
             save_file(graph, f"graph{i}.pdf")
---
+++ MLTools/DecisionTree/src/_SaveGraph.py
@@ -0,0 +1,157 @@
+def _write_canon(graph, filename):
+    graph.write_canon(filename)
+
+def _write_cmap(graph, filename):
+    graph.write_cmap(filename)
+
+def _rite_cmapx(graph, filename):
+    graph.write_cmapx(filename)
+
+def _write_cmapx_np(graph, filename):
+    graph.write_cmapx_np(filename)
+
+def _write_dia(graph, filename):
+    graph.write_dia(filename)
+
+def _write_dot(graph, filename):
+    graph.write_dot(filename)
+
+def _write_fig(graph, filename):
+    graph.write_dot(filename)
+
+def _write_gd(graph, filename):
+    graph.write_fig(filename)
+
+def _write_gd2(graph, filename):
+    graph.write_gd2(filename)
+
+def _write_gif(graph, filename):
+    graph.write_gif(filename)
+
+def _write_hpgl(graph, filename):
+    graph.write_hpgl(filename)
+
+def _write_imap(graph, filename):
+    graph.write_imap(filename)
+
+def _write_imap_np(graph, filename):
+    graph.write_imap_np(filename)
+
+def _write_ismap(graph, filename):
+    graph.write_ismap(filename)
+
+def _write_jpe(graph, filename):
+    graph.write_jpe(filename)
+
+def _write_jpeg(graph, filename):
+    graph.write_jpeg(filename)
+
+def _write_jpg(graph, filename):
+    graph.write_jpg(filename)
+
+def _write_mif(graph, filename):
+    graph.write_mif(filename)
+
+def _write_mp(graph, filename):
+    graph.write_mp(filename)
+
+def _write_pcl(graph, filename):
+    graph.write_pcl(filename)
+
+def _write_pdf(graph, filename):
+    graph.write_pdf(filename)
+
+def _write_pic(graph, filename):
+    graph.write_pic(filename)
+
+def _write_plain(graph, filename):
+    graph.write_plain(filename)
+
+# def _write_plain_ext(graph, filename):
+#     graph.(filename)
+
+def _write_png(graph, filename):
+    graph.write_png(filename)
+
+def _write_ps(graph, filename):
+    graph.write_ps(filename)
+
+def _write_ps2(graph, filename):
+    graph.write_ps2(filename)
+
+def _write_raw(graph, filename):
+    graph.write_raw(filename)
+
+def _write_svg(graph, filename):
+    graph.canon(filename)
+
+def _write_svgz(graph, filename):
+    graph.canon(filename)
+
+def _write_vml(graph, filename):
+    graph.canon(filename)
+
+def _write_vmlz(graph, filename):
+    graph.canon(filename)
+
+def _write_vrml(graph, filename):
+    graph.canon(filename)
+
+def _write_vtx(graph, filename):
+    graph.canon(filename)
+
+def _write_wbmp(graph, filename):
+    graph.canon(filename)
+
+def _write_xdot(graph, filename):
+    graph.canon(filename)
+
+def _write_xlib(graph, filename):
+    graph.canon(filename)
+
+
+func_dict = {
+    "canon": _write_canon,
+    "cmap": _write_cmap,
+    "cmapx": _rite_cmapx,
+    "cmapx_np": _write_cmapx_np,
+    "dia": _write_dia,
+    "dot": _write_dot,
+    "fig": _write_fig,
+    "gd": _write_gd,
+    "gd2": _write_gd2,
+    "gif": _write_gif,
+    "hpgl": _write_hpgl,
+    "imap": _write_imap,
+    "imap_np": _write_imap_np,
+    "ismap": _write_ismap,
+    "jpe": _write_jpe,
+    "jpeg": _write_jpeg,
+    "jpg": _write_jpg,
+    "mif": _write_mif,
+    "mp": _write_mp,
+    "pcl": _write_pcl,
+    "pdf": _write_pdf,
+    "pic": _write_pic,
+    "plain": _write_plain,
+    # "plain_ext": _write_plain_ext,
+    "png": _write_png,
+    "ps": _write_ps,
+    "ps2": _write_ps2,
+    "raw": _write_raw,
+    "svg": _write_svg,
+    "svgz": _write_svgz,
+    "vml": _write_vml,
+    "vmlz": _write_vmlz,
+    "vrml": _write_vrml,
+    "vtx": _write_vtx,
+    "vbmp": _write_wbmp,
+    "xdot": _write_xdot,
+    "xlib": _write_xlib
+}
+def write_file(graph, filename, mode):
+    func_dict[mode](graph, filename)
+
+
+def get_format_list():
+    return list(func_dict.keys())
